b8359e2802749a6c1ace6afb0c8d19fa46771f67
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-06-11 09:27:03 +0100

    Sencha command wrapper now fails correctly if sencha command fails or does not exist

--- sencha_wrapper.py
+++ sencha_wrapper.py
@@ -13,23 +13,30 @@ def get_errors(s):
     return ret
 
 print('Running Sencha command...')
-proc = subprocess.Popen(['sencha.bat' if os.name == 'nt' else 'sencha'] + list(sys.argv[1:]),
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE)
-stdout, stderr = proc.communicate()
+try:
+    proc = subprocess.Popen(['sencha.bat' if os.name == 'nt' else 'sencha'] + list(sys.argv[1:]),
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
 
-if proc.returncode != 0 or contains_errors(stdout) or contains_errors(stderr):
-    return_code = proc.returncode or 1
-    sys.stderr.write('Command failed\n')
-else:
-    return_code = 0
+    if proc.returncode != 0 or contains_errors(stdout) or contains_errors(stderr):
+        return_code = proc.returncode or 1
+        sys.stderr.write('Command failed\n')
+    else:
+        return_code = 0
 
-sys.stdout.write(stdout)
-sys.stderr.write(stderr)
+    sys.stdout.write(stdout)
+    sys.stderr.write(stderr)
+except Exception as e:
+    stdout = ''
+    stderr = ('[ERROR] Failed to execute sencha command, did you reboot and ensure the sencha command is always on the '
+              'PATH? (%s)' % str(e))
+    return_code = 2
 
 # Eclipse does not seem to stop the build even for return codes != 1, so let's be a bit more cruel
 with open(os.path.join('android', 'AndroidManifest.xml'), 'r+t') as f:
-    MAGIC = 'SENCHA BUILD FAILED, PLEASE CHECK FOR ERRORS AND RE-RUN BUILD (THIS LINE IS REMOVED AUTOMATICALLY IF SENCHA BUILD SUCCEEDS)'
+    MAGIC = ('SENCHA BUILD FAILED, PLEASE CHECK FOR ERRORS AND RE-RUN BUILD (THIS LINE IS REMOVED AUTOMATICALLY IF '
+             'SENCHA BUILD SUCCEEDS)')
     content = f.read()
 
     magicPosition = content.find(MAGIC)
